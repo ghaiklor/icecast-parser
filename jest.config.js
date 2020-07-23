70efe5bf274336391d2c4933847416a15c7881da
AlexanderTirik <sasha.tirik@gmail.com>
2020-07-23 18:51:04 +0300

    Fixed

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["json", "text", "lcov", "clover"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
   errorOnDeprecated: true,
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
 };
