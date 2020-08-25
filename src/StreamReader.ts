b7a360ea20196146f9783cc37310a096ce8c9895
Eugene Obrezkov <ghaiklor@gmail.com>
2020-08-25 08:38:26 +0300

    refactor: 💡 port sources to TypeScript

---
+++ src/StreamReader.ts
@@ -0,0 +1,141 @@
+import { Transform } from 'stream';
+
+const METADATA_BLOCK_SIZE = 16;
+const METADATA_REGEX = /<?key(\w+))=['"]<?value(.+?)>['"];/gu;
+
+const enum STATES {
+  INIT_STATE,
+  BUFFERING_STATE,
+  PASSTHROUGH_STATE
+}
+
+type Trampoline<T> = T | ((...args: never[]) => Trampoline<T>);
+type TransformCallback = (error: Error | null, data?: Buffer) => void;
+
+const parseMetadata = (metadata: Buffer | string): Map<string, string> => {
+  const map = new Map<string, string>();
+  const data = Buffer.isBuffer(metadata) ? metadata.toString('utf8') : metadata || '';
+  const parts = [...data.replace(/\0*$/u, '').matchAll(METADATA_REGEX)];
+
+  parts.forEach((part) => map.set(part.groups?.key ?? '', part.groups?.value ?? ''));
+
+  return map;
+};
+
+function trampoline <
+  T,
+  P extends never[],
+  R extends Trampoline<T>,
+  F extends (...args: P) => R
+> (fn: F): (...args: Parameters<F>) => ReturnType<F> {
+  return function executor (...args: Parameters<F>): ReturnType<F> {
+    let result = fn(...args);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    while (typeof result === 'function') result = result();
+    return result as ReturnType<F>;
+  };
+}
+
+const processData = (stream: StreamReader, chunk: Buffer, done: TransformCallback): TransformCallback => {
+  stream.bytesLeft -= chunk.length;
+
+  if (stream.currentState === STATES.BUFFERING_STATE) {
+    stream.buffers.push(chunk);
+    stream.buffersLength += chunk.length;
+  } else if (stream.currentState === STATES.PASSTHROUGH_STATE) {
+    stream.push(chunk);
+  }
+
+  if (stream.bytesLeft === 0) {
+    const cb = stream.callback;
+
+    let newChunk: Buffer = Buffer.from('');
+    if (cb && stream.currentState === STATES.BUFFERING_STATE && stream.buffers.length > 1) {
+      newChunk = Buffer.concat(stream.buffers, stream.buffersLength);
+    }
+
+    stream.currentState = STATES.INIT_STATE;
+    stream.callback = null;
+    stream.buffers.splice(0);
+    stream.buffersLength = 0;
+
+    cb?.call(stream, newChunk);
+  }
+
+  return done;
+};
+
+const onData = trampoline((stream: StreamReader, chunk: Buffer, done: TransformCallback): () => TransformCallback => {
+  if (chunk.length <= stream.bytesLeft) {
+    return (): TransformCallback => processData(stream, chunk, done);
+  }
+
+  return (): TransformCallback => {
+    const buffer = chunk.slice(0, stream.bytesLeft);
+
+    return processData(stream, buffer, (error): unknown => {
+      if (error) return done(error);
+
+      if (chunk.length > buffer.length) {
+        return (): TransformCallback => onData(stream, chunk.slice(buffer.length), done);
+      }
+
+      return done;
+    });
+  };
+});
+
+export class StreamReader extends Transform {
+  public buffers: Buffer[] = [];
+  public buffersLength = 0;
+  public bytesLeft = 0;
+  public callback: ((chunk: Buffer) => void) | null = null;
+  public currentState = STATES.INIT_STATE;
+  public readonly icyMetaInt: number = 0;
+
+  public constructor (icyMetaInt: number) {
+    super();
+
+    this.icyMetaInt = icyMetaInt;
+    this.passthrough(this.icyMetaInt, this.onMetaSectionStart.bind(this));
+  }
+
+  public transform (chunk: Buffer, _encoding: string, done: TransformCallback): void {
+    onData(this, chunk, done);
+  }
+
+  protected bytes (length: number, cb: (chunk: Buffer) => void): this {
+    this.bytesLeft = length;
+    this.currentState = STATES.BUFFERING_STATE;
+    this.callback = cb;
+
+    return this;
+  }
+
+  protected passthrough (length: number, cb: (chunk: Buffer) => void): this {
+    this.bytesLeft = length;
+    this.currentState = STATES.PASSTHROUGH_STATE;
+    this.callback = cb;
+
+    return this;
+  }
+
+  protected onMetaSectionStart (): void {
+    this.bytes(1, this.onMetaSectionLengthByte.bind(this));
+  }
+
+  protected onMetaSectionLengthByte (chunk: Buffer): void {
+    const length = chunk[0] * METADATA_BLOCK_SIZE;
+
+    if (length > 0) {
+      this.bytes(length, this.onMetaData.bind(this));
+    } else {
+      this.passthrough(this.icyMetaInt, this.onMetaSectionStart.bind(this));
+    }
+  }
+
+  protected onMetaData (chunk: Buffer): void {
+    this.emit('metadata', parseMetadata(chunk));
+    this.passthrough(this.icyMetaInt, this.onMetaSectionStart.bind(this));
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import { EventEmitter } from 'events';
+import { StreamReader } from './StreamReader';
+import http from 'http';
+import https from 'https';
+
+export interface Options {
+  autoUpdate: boolean;
+  emptyInterval: number;
+  errorInterval: number;
+  keepListen: boolean;
+  metadataInterval: number;
+  notifyOnChangeOnly: boolean;
+  url: string;
+  userAgent: string;
+}
+
+export class RadioParser extends EventEmitter {
+  private previousMetadata = '';
+  private readonly options: Options = {
+    autoUpdate: true,
+    emptyInterval: 5 * 60,
+    errorInterval: 10 * 60,
+    keepListen: false,
+    metadataInterval: 5,
+    notifyOnChangeOnly: false,
+    url: '',
+    userAgent: 'icecast-parser'
+  };
+
+  public constructor (options: Partial<Options>) {
+    super();
+
+    this.options = { ...this.options, ...options };
+    this.queueRequest();
+  }
+
+  protected onRequestResponse (response: http.IncomingMessage): void {
+    const icyMetaInt = response.headers['icy-metaint'];
+
+    if (typeof icyMetaInt === 'undefined') {
+      this.destroyResponse(response);
+      this.queueNextRequest(this.options.emptyInterval);
+      this.emit('empty');
+    } else {
+      const reader = new StreamReader(Array.isArray(icyMetaInt) ? Number(icyMetaInt[0]) : Number(icyMetaInt));
+
+      reader.on('metadata', (metadata) => {
+        this.destroyResponse(response);
+        this.queueNextRequest(this.options.metadataInterval);
+
+        const newMetadata = JSON.stringify(metadata);
+        if (this.options.notifyOnChangeOnly && newMetadata !== this.previousMetadata) {
+          this.emit('metadata', metadata);
+          this.previousMetadata = newMetadata;
+        } else if (!this.options.notifyOnChangeOnly) {
+          this.emit('metadata', metadata);
+        }
+      });
+
+      response.pipe(reader);
+      this.emit('stream', reader);
+    }
+  }
+
+  protected onRequestError (error: Error): void {
+    this.queueNextRequest(this.options.errorInterval);
+    this.emit('error', error);
+  }
+
+  protected onSocketEnd (): void {
+    if (this.options.keepListen) {
+      this.emit('end');
+    }
+  }
+
+  protected makeRequest (): void {
+    const request = this.options.url.startsWith('https://')
+      ? https.request(this.options.url)
+      : http.request(this.options.url);
+
+    request.setHeader('Icy-MetaData', '1');
+    request.setHeader('User-Agent', this.options.userAgent);
+    request.once('socket', (socket) => socket.once('end', this.onSocketEnd.bind(this)));
+    request.once('response', this.onRequestResponse.bind(this));
+    request.once('error', this.onRequestError.bind(this));
+    request.end();
+  }
+
+  protected destroyResponse (response: http.IncomingMessage): void {
+    if (!this.options.keepListen) {
+      response.destroy();
+    }
+  }
+
+  protected queueNextRequest (timeout: number): void {
+    if (this.options.autoUpdate && !this.options.keepListen) {
+      this.queueRequest(timeout);
+    }
+  }
+
+  protected queueRequest (timeout = 0): void {
+    setTimeout(this.makeRequest.bind(this), timeout * 1000);
+  }
+}
