1b6d3cb3a2e060fd4ad8f8d76dc33403c225dff2
Eugene Obrezkov <ghaiklor@gmail.com>
2020-09-20 16:34:40 +0300

    test: 💍 when the radio have no support we need to emit empty

--- test/Parser.spec.ts
+++ test/Parser.spec.ts
@@ -1,12 +1,31 @@
 import { Parser } from '../src/Parser';
 
 describe('parser', () => {
-  it('should properly emit metadata from icecast', async () => await new Promise((resolve) => {
+  it('should properly emit metadata event from the radio', async () => await new Promise((resolve) => {
     expect.hasAssertions();
 
     const radio = new Parser({ autoUpdate: false, url: 'https://live.hunter.fm/80s_high' });
     radio.on('metadata', (metadata) => {
+      expect(metadata.size).toBe(2);
       expect(metadata.get('StreamTitle')).toStrictEqual(expect.any(String));
+      expect(metadata.get('StreamUrl')).toStrictEqual(expect.any(String));
+      resolve();
+    });
+  }));
+
+  it('should properly emit empty event from the radio when no support', async () => await new Promise((resolve) => {
+    expect.hasAssertions();
+
+    let isMetadataFired = false;
+
+    const radio = new Parser({ autoUpdate: false, url: 'http://stream-dc1.radioparadise.com:80/ogg-96m' });
+
+    radio.on('metadata', (_metadata) => {
+      isMetadataFired = true;
+    });
+
+    radio.on('empty', () => {
+      expect(isMetadataFired).toBe(false);
       resolve();
     });
   }));
