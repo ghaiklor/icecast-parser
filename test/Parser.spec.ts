d03b37924e95b529e8d1fc8e40fe6a9f0e5027fb
Eugene Obrezkov <ghaiklor@gmail.com>
2020-09-20 16:51:21 +0300

    test: 💍 when there is a network error it should emit error

--- test/Parser.spec.ts
+++ test/Parser.spec.ts
@@ -29,4 +29,28 @@ describe('parser', () => {
       resolve();
     });
   }));
+
+  it('should properly emit error event if request has failed', async () => await new Promise((resolve) => {
+    expect.hasAssertions();
+
+    let isMetadataFired = false;
+    let isEmptyFired = false;
+
+    const radio = new Parser({ autoUpdate: false, url: 'http://non-existing-url.com' });
+
+    radio.on('metadata', (_metadata) => {
+      isMetadataFired = true;
+    });
+
+    radio.on('empty', () => {
+      isEmptyFired = true;
+    });
+
+    radio.on('error', (error) => {
+      expect(isMetadataFired).toBe(false);
+      expect(isEmptyFired).toBe(false);
+      expect(error).toMatchObject({ errno: -3008, hostname: 'non-existing-url.com', syscall: 'getaddrinfo' });
+      resolve();
+    });
+  }));
 });
