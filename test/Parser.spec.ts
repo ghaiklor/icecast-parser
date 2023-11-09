e5690160876b8cb4129126c89e5288a8ed6c4da0
Beastlybear2017 <96798368+Beastlybear2017@users.noreply.github.com>
2023-11-09 15:45:47 +0000

    Update Parser.spec.ts

--- test/Parser.spec.ts
+++ test/Parser.spec.ts
@@ -1,7 +1,7 @@
 import { Parser } from '../src/Parser';
 
 describe('parser', () => {
-  it('should properly emit metadata event from the radio', async () => await new Promise((resolve) => {
+  it('should properly emit metadata event from the radio', async () => await new Promise<void>((resolve) => {
     expect.hasAssertions();
 
     const radio = new Parser({ autoUpdate: false, url: 'https://live.hunter.fm/80s_high' });
@@ -13,7 +13,7 @@ describe('parser', () => {
     });
   }));
 
-  it('should properly emit empty event from the radio when no support', async () => await new Promise((resolve) => {
+  it('should properly emit empty event from the radio when no support', async () => await new Promise<void>((resolve) => {
     expect.hasAssertions();
 
     let isMetadataFired = false;
@@ -30,7 +30,7 @@ describe('parser', () => {
     });
   }));
 
-  it('should properly emit error event if request has failed', async () => await new Promise((resolve) => {
+  it('should properly emit error event if request has failed', async () => await new Promise<void>((resolve) => {
     expect.hasAssertions();
 
     let isMetadataFired = false;
@@ -54,14 +54,44 @@ describe('parser', () => {
     });
   }));
 
-  it('should properly emit metadata event when metadata has been updated', async () => await new Promise((resolve) => {
+  it('should properly emit metadata event when metadata has been updated', async () => await new Promise<void>((resolve) => {
     expect.hasAssertions();
 
-    const radio = new Parser({ autoUpdate: false, notifyOnChangeOnly: true, url: 'https://live.hunter.fm/80s_high' });
+    const radio = new Parser({ autoUpdate: false, notifyOnChangeOnly: [], url: 'https://live.hunter.fm/80s_high' });
     radio.on('metadata', (metadata) => {
       // @ts-expect-error I want to check that metadata was stored in the private property to later comparsion
       expect(radio.previousMetadata).toStrictEqual(metadata);
       resolve();
     });
   }));
+
+  it('should properly emit metadata event when notifyOnChangeOnly is used and when metadata has been updated', async () => await new Promise<void>((resolve) => {
+    expect.hasAssertions()
+
+    const radio = new Parser({ autoUpdate: false, notifyOnChangeOnly: ['StreamTitle'], url: 'https://live.hunter.fm/80s_high' });
+    radio.on('metadata', (metadata) => {
+      // @ts-expect-error I want to check that metadata was stored in the private property to later comparsion
+      expect(radio.previousMetadata).toStrictEqual(metadata);
+      resolve();
+    });
+  }))
+
+  it('should not emmit metadata event if notifyOnChangeOnly is incorrect', async () => await new Promise<void>(async (resolve) => {
+    // expect.hasAssertions()
+    let triggered = false
+    const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+    const radio = new Parser({ autoUpdate: false, notifyOnChangeOnly: ['a'], url: 'https://live.hunter.fm/80s_high' });
+    radio.on('metadata', () => {
+      triggered = true
+    });
+
+    await delay(1000)
+
+    if(!triggered) {
+      resolve()
+    } else {
+      throw new Error('metadata event was triggered - Check that the notifyOnChangeOnly data does not exist in the metadata')
+    }
+  }))
 });
